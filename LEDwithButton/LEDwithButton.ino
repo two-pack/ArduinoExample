97af8691c0eaf0020275241effc5fb34ab629df9
Tatsuya Saito <twopackas@gmail.com>
2014-07-14 20:13:54 +0900

    Switches 2 LEDs.

--- LEDwithButton/LEDwithButton.ino
+++ LEDwithButton/LEDwithButton.ino
@@ -1,17 +1,21 @@
 // Turn on and off LED with button.
-const int LED = 13;
+const int RedLED = 13;
+const int GreenLED = 11;
 const int BUTTON = 7;
 
 static int buttonState = LOW;
 static int beforeButtonState = LOW;
-static int LEDState = 0;  // 0: off, 1: on
+static int LEDState = 0;  // 0: off, 1: Red, 2: Green, 3: Both
 
 void updateLEDState()
 {
   buttonState = digitalRead(BUTTON);
   if(buttonState == HIGH) {
     if(beforeButtonState == LOW) {
-      LEDState = 1 - LEDState;  // switch LED.
+      LEDState++;  // switch LED.
+      if(LEDState > 3) {
+        LEDState = 0;
+      }
       beforeButtonState = HIGH;
       delay(10);  //debouncing;
     }
@@ -22,17 +26,34 @@ void updateLEDState()
 
 void setup()
 {
-  initialize(LED);
+  initialize(RedLED);
+  initialize(GreenLED);
   pinMode(BUTTON, INPUT);
 }
 
 void loop()
 {
   updateLEDState();
-  if(LEDState) {
-    turnOnLED(LED);
-  } else {
-    turnOffLED(LED);
+  switch(LEDState) {
+    case 0:
+      turnOffLED(RedLED);
+      turnOffLED(GreenLED);
+      break;
+      
+    case 1:
+      turnOnLED(RedLED);
+      turnOffLED(GreenLED);
+      break;
+
+    case 2:
+      turnOffLED(RedLED);
+      turnOnLED(GreenLED);
+      break;
+
+    case 3:
+      turnOnLED(RedLED);
+      turnOnLED(GreenLED);
+      break;
   }
 }
 
