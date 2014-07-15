3e5de68eef62cb1b1e4190122887a5e0c33ea14f
Tatsuya Saito <twopackas@gmail.com>
2014-07-15 12:13:09 +0900

    Controls LED with PWM and button.

---
+++ LEDwithPWMandButton/LEDwithPWMandButton.ino
@@ -0,0 +1,36 @@
+// LED with PWM and Button.
+const int LED = 9;
+const int BUTTON = 7;
+int i = 0;
+
+void operateLED(int value)
+{
+  analogWrite(LED, value);
+  delay(10);
+}
+
+void turnOffLED()
+{
+  operateLED(0);
+  i = 0;
+  delay(10);
+}
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, OUTPUT);
+}
+
+void loop() {
+  if(digitalRead(BUTTON) == LOW) {
+    turnOffLED();
+  } else {
+    if(i < 255) {
+      i++;
+    } else {
+      i = 0;
+    }
+    operateLED(i);
+  }
+}
+
